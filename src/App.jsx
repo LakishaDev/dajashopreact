9a77ae4a8db341b4366a404b37ca4efaac468e55
Lazar <lazar.cve@gmail.com>
2025-11-19 16:59:36 +0100

    feat: add Lenis smooth scrolling and update AdminProductModal for better UX

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import React from "react";
 import AppRoutes from "./router.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import AuthModal from "./components/AuthModal.jsx";
 
+import { ReactLenis } from "lenis/react";
+import { useEffect, useRef } from "react";
+
 export default function App() {
+  const lenisRef = useRef();
+
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time);
+    }
+
+    const rafId = requestAnimationFrame(update);
+
+    return () => cancelAnimationFrame(rafId);
+  }, []);
+
   return (
     <div className="app-root">
+      <ReactLenis
+        root
+        options={{
+          lerp: 0.08, // smoothness (0 - 1)
+          smoothWheel: true, // enables smooth for mouse/touchpad
+          autoRaf: true, // automatski animira
+          anchors: true, // omogućava glatko skrolovanje do anchor linkova
+          touchMultiplier: 0.5, // povećava brzinu skrolovanja na touch uređajima
+        }}
+        ref={lenisRef}
+      />
       <Header />
       <main className="container" style={{ padding: "20px 0 48px" }}>
         <AuthModal />
